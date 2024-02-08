fd77a5663a8787560aedc7c7f9e0ddd05df9711a
Kubi Dogan <kubi_g@hotmail.co.uk>
2024-02-08 21:44:35 +0000

    rails/all

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 require "active_support/core_ext/integer/time"
+require 'sprockets/railtie'
+require 'rails/all'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
