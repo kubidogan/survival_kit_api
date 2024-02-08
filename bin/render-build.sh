a9066b6fb30919a002f9b7a697d1a90288f76d7c
Kubi Dogan <kubi_g@hotmail.co.uk>
2024-02-08 22:20:00 +0000

    seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rake db:seed
 
 # bundle install
 # ./bin/rails assets:precompile
