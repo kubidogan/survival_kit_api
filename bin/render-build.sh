fbce948a21441564b6169df1dc843d590eb7a982
Kubi Dogan <kubi_g@hotmail.co.uk>
2024-02-08 21:57:33 +0000

    assets

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
-
 # bundle install
-# ./bin/rails assets:precompile
-# ./bin/rails assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
+
+bundle install
+./bin/rails assets:precompile
+./bin/rails assets:clean
